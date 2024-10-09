d0c5eff4c72ec7d010fd8a16a6a744739d5d324f
leonardopadilha <leonardopadilha28@gmail.com>
2024-10-09 06:34:39 -0300

    Listando um ou mais games

--- api-rest/introducao/index.js
+++ api-rest/introducao/index.js
@@ -13,8 +13,25 @@ const DB = {
     ]
 }
 
-app.get('/', () => {
-    
+app.get('/games', (req, res) => {
+    res.statusCode = 200;
+    res.json(DB.games);
+});
+
+app.get('/games/:id', (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400)        
+    }else {
+        const id = parseInt(req.params.id);
+        const game = DB.games.find(game => game.id === id);
+
+        if (game != undefined) {
+            res.statusCode = 200;
+            res.json(game)
+        }else {
+            res.sendStatus(404);
+        }
+    }
 })
 
 app.listen(45678, () => {
