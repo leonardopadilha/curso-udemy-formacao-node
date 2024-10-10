bd4fa7c4d0947c6879a8e19ec976b3e39fa6a396
leonardopadilha <leonardopadilha28@gmail.com>
2024-10-10 06:22:40 -0300

    Iniciando a utilização do axios

--- api-rest/introducao/index.js
+++ api-rest/introducao/index.js
@@ -34,6 +34,57 @@ app.get('/games/:id', (req, res) => {
     }
 })
 
+app.post('/games', (req, res) => {
+    const { title, price, year } = req.body
+
+    DB.games.push({
+        id: 2323,
+        title: title,
+        year: year,
+        price: price
+    })
+
+    res.statusCode = 201;
+    res.json(DB.games)
+})
+
+app.delete('/games/:id', (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400)        
+    }else {
+        const id = parseInt(req.params.id);
+        const game = DB.games.findIndex(game => game.id === id);
+
+        if (game == -1) {
+            res.sendStatus(404);
+        }else {
+            DB.games.splice(game, 1);
+            res.sendStatus(200)
+        }
+    }
+})
+
+app.put('/games/:id', (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400)        
+    }else {
+        const id = parseInt(req.params.id);
+        const game = DB.games.find(game => game.id === id);
+
+        if (game != undefined) {
+            const { title, price, year } = req.body
+
+            if (title != undefined) game.title = title
+            if (price != undefined) game.price = price
+            if (year != undefined) game.year = year
+        res.sendStatus(200)
+        
+        }else {
+            res.sendStatus(404);
+        }
+    }
+})
+
 app.listen(45678, () => {
     console.log('API RODANDO!!!')
 })
